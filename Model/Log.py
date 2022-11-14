378077fe577f1ad66ad568eb46ee9487f21ac601
s184209 <s184209@student.dtu.dk>
2022-11-14 20:36:50 +0100

    accuracy, sensitivity and precision is now calculated for logged data

--- Model/Log.py
+++ Model/Log.py
@@ -7,15 +7,21 @@ sys.path.append(os.path.dirname(__file__))
 
 class Log():
     def __init__(self):
-        self.header = ['time', 'fireArea', 'predictedFireArea', 'precision']
+        self.header = ['time', 'fireArea', 'predictedFireArea', 'accuracy', 'sensitivity', 'specificity']
         self.log = []
 
     def add(self, time, fireMap, predictedFireMap):
         fireArea = np.count_nonzero(fireMap)
         predictedFireArea = np.count_nonzero(predictedFireMap)
-        # should count true positives and false positives as well as true negatives and false negatives
-        precision = (fireArea - abs(fireArea - predictedFireArea)) / fireArea
-        self.log.append([int(time), fireArea, predictedFireArea, precision])
+        # formulas from https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4614595/
+        tp = self.get_true_positive(fireMap, predictedFireMap)
+        fp = self.get_false_positive(fireMap, predictedFireMap)
+        tn = self.get_true_negative(fireMap, predictedFireMap)
+        fn = self.get_false_negative(fireMap, predictedFireMap)
+        accuracy = (tp + tn) / (tp + fp + tn + fn)
+        sensitivity = tp / (tp + fn)
+        specificity = tn / (tn + fp)
+        self.log.append([int(time), fireArea, predictedFireArea, accuracy, sensitivity, specificity])
     
     def write(self, seed, n, m, droneCount):
         filename = 'log_' + str(seed) + '_' + str(n) + '_' + str(m) + '_' + str(droneCount) + '.csv'
@@ -24,3 +30,15 @@ class Log():
             writer.writerow(self.header)
             for row in self.log:
                 writer.writerow(row)
+
+    def get_true_positive(self, fireMap, predictedFireMap):
+        return np.count_nonzero(np.logical_and(fireMap, predictedFireMap))
+    
+    def get_false_positive(self, fireMap, predictedFireMap):
+        return np.count_nonzero(np.logical_and(np.logical_not(fireMap), predictedFireMap))
+
+    def get_true_negative(self, fireMap, predictedFireMap):
+        return np.count_nonzero(np.logical_and(np.logical_not(fireMap), np.logical_not(predictedFireMap)))
+
+    def get_false_negative(self, fireMap, predictedFireMap):
+        return np.count_nonzero(np.logical_and(fireMap, np.logical_not(predictedFireMap)))
