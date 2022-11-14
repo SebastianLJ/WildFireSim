02a0e585367c63e2a580062285cfd16d9d9f125e
s184209 <s184209@student.dtu.dk>
2022-11-14 14:19:14 +0100

    added log class

---
+++ Model/Log.py
@@ -0,0 +1,25 @@
+import csv
+import numpy as np
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.dirname(__file__))
+
+class Log():
+    def __init__(self):
+        self.header = ['time', 'fireArea', 'predictedFireArea', 'precision']
+        self.log = []
+
+    def add(self, time, fireMap, predictedFireMap):
+        fireArea = np.count_nonzero(fireMap)
+        predictedFireArea = np.count_nonzero(predictedFireMap)
+        # should count true positives and false positives as well as true negatives and false negatives
+        precision = (fireArea - (fireArea - predictedFireArea)) / fireArea
+        self.log.append([time, fireArea, predictedFireArea, precision])
+    
+    def write(self, filename):
+        with open(filename, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow(self.header)
+            for row in self.log:
+                writer.writerow(row)
